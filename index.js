fc7eb9fce5bacc84779088a3b9bb11134d90edde
kwyptalt <kryptopalette@gmail.com>
2024-12-08 20:10:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["The Ghost.Org!", "The Ghost.Org!"];
+const statusMessages = ["Managing all DHS operations.", "Managing all DHS operations."];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
