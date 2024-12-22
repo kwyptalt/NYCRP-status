b304335cbb6930f1f0d0224dea68cd0f16922f6d
kwyptalt <kryptopalette@gmail.com>
2024-12-22 20:21:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["MANAGING STAFF: New York City Roleplay!", "MANAGING STAFF: New York City Roleplay!"];
+const statusMessages = ["MANAGING STAFF: New York City Roleplay!", "MANAGING GAME: New York City Roleplay!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
